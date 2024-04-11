802dd513603146142fa9fdd4b9241d63331f075f
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-04-11 20:18:15 +0200

    Przykłady asercji 4

--- unit-testing-intro/spec/appSpec.js
+++ unit-testing-intro/spec/appSpec.js
@@ -4,14 +4,23 @@ var app = require('../app/app');
 // tworzymy pierwszy test
 
 describe('Math', function(){
+    //sprawdza sumowanie
     it('should return 10 when 5 and 5 are given', function(){
         expect(sum(5,5)).toEqual(10);
     });
+    //sprawdza zwrot TRUE
     it('should return true', function(){
         expect(returnTrue()).toBeTruthy();
     });
+    //sprawdza zwrot UNDEFINED
     it('should return undefined', function(){
-        expect(returnUndefined()).toBeUndefined();
+        expect(returnUndefined()).toBeUndefined();// "not" przed ".toBe" jest zaprzeczeniem.
+    });
+    //sprawdza identyczność
+    it('should compare two objects', function(){
+        var a = {};
+        var b = {};
+        expect(a).toEqual(b);//używając toBe nie zaliczy testu
     });
     
 });
