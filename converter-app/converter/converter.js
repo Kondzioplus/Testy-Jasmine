c683c0f21c6f173f574420a66be6b79cf97b413f
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-04-18 17:22:43 +0200

    Testujemy funkcje asynchroniczne

--- converter-app/converter/converter.js
+++ converter-app/converter/converter.js
@@ -1,5 +1,8 @@
-// Zalicza test zwracając OBJECT.
 
-getLatestRates = function(){
-    return {};
-} 
+var request = require('request');
+getLatestRates = function(callback){
+    request('http://api.fixer.io/latest', function(error, response, body){
+        var ratesList = JSON.parse(body);
+        return callback(ratesList);
+    });
+}; 
--- converter-app/spec/converterSpec.js
+++ converter-app/spec/converterSpec.js
@@ -1,11 +1,14 @@
-// Przygotowanie testu funkcji.
+// Testujemy funckje asynchroniczne.
 var converter = require('../converter/converter');
 
 describe('Currency Converter', function(){
     describe('GET echange rates', function(){
-        it('should return object', function(){
-            var result = getLatestRates();
-            expect(typeof result).toEqual('object');
+        it('should return object', function(done){
+            getLatestRates(function(result){
+                expect(typeof result).toEqual('object');
+                done();//wbudowany pakiet jasmine dla asynchroniczności.
+            });
+            
         });
     });
 });
